913bd7041f568472fd9cf9960dc0bc558fc8f3e5
Tomiwa <47110661+a-tomiwa@users.noreply.github.com>
2020-08-31 22:37:34 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        square(1,3);
+        square(5,6);
+        square(7, 10);
+
+    }
+
+    public static void square(int x, int y) {
+        for (int i = x; i <= y; i++){
+            System.out.println("> i- " + i*i);
+
+        }
+
+
+    }
+}
